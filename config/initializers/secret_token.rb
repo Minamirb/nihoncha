ce9ceb36d25480617724608ccb5dd32218397373
shoko.mantani <shoko@ubuntu-vbox.(none)>
2010-11-23 15:34:08 +0900

    空のrails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nihoncha::Application.config.secret_token = '6faf4a8c9e95f169c70b4da24918c9cc68be40b7c27defac29592977869f4f6394840afb79f7b13e388307b0c03305506fc2fa8774683d48d19a796de3ea0d26'
