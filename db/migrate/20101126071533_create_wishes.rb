14c7d080d187ade2967e9b649d5410aa17542075
yalab <rudeboyjet@gmail.com>
2010-11-26 16:16:09 +0900

    Wish モデルをgenerate

---
+++ db/migrate/20101126071533_create_wishes.rb
@@ -0,0 +1,19 @@
+class CreateWishes < ActiveRecord::Migration
+  def self.up
+    create_table :wishes do |t|
+      t.string :uri
+      t.string :name
+      t.integer :price
+      t.string :image_url
+      t.string :thumbneil_url
+      t.boolean :bought
+      t.references :user
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :wishes
+  end
+end
