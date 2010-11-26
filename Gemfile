cd4762448932715601d5a460c52c22abb4e88f20
yalab <rudeboyjet@gmail.com>
2010-11-26 19:53:31 +0900

    Implement Wish#fetch.

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'jquery-rails'
 gem 'mocha'
 gem 'shoulda'
+gem 'nokogiri'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
 class Wish < ActiveRecord::Base
   belongs_to :user
+
+  def fetch
+    html = nil
+    doc = Nokogiri::HTML(open(uri).read)
+    self.name = doc.xpath('//title').first.text.split(/\s*[:：]\s*/)[1]
+    self.price = doc.xpath("//b[@class='priceLarge']").first.text.slice(/[\d,]+/).delete(',').to_i
+    self.description = doc.xpath("//div[@class='productDescriptionWrapper']").first.text
+  end
+
 end
